b7f0849007bdc895ca7e380a86fc1896e115bf9b
melissaran <39605081+melissaran@users.noreply.github.com>
2023-07-08 15:33:31 -0700

    move code to possessable

--- game/Assets/Scripts/Possessable.cs
+++ game/Assets/Scripts/Possessable.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Possessable : MonoBehaviour
 {
+    
+    public float distance = 3;
+    public float thresh = 0.5f;
+    public Rigidbody2D platform;
+
+    public float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //dummy platform move
+
+        platform.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+
+        Vector2 vec = -platform.transform.localPosition;
+        float dist = vec.magnitude;
+        float fac = Mathf.Clamp(dist - distance, 0, thresh) / thresh;
+        platform.velocity += vec.normalized * fac * speed;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Unpossess();
+        }
+    }
+
+    void Unpossess()
+    {
+        Transform player = GameObject.FindObjectOfType<PlayerController>().transform;
+        player.position = platform.transform.position + Vector3.up*0.2f;
+        player.gameObject.SetActive(true);
+        this.enabled = false;
     }
 }
